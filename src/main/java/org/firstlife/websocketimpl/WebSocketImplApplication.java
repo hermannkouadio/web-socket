c098e1ecdae78c80bb9ccac8f135b318c0010531
Hermann KOUADIO <hermann@boanergues.home.org>
2024-07-04 03:34:08 +0200

    intial commit

---
+++ src/main/java/org/firstlife/websocketimpl/WebSocketImplApplication.java
@@ -0,0 +1,13 @@
+package org.firstlife.websocketimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/firstlife/websocketimpl/WebSocketImplApplicationTests.java
@@ -0,0 +1,13 @@
+package org.firstlife.websocketimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
